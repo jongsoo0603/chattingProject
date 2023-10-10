5c460e20657fc60299afe9fcec8bfed2596068aa
seguir0114 <hbinch9598@gmail.com>
2023-10-10 15:57:37 +0900

    add joinMembership func

--- chattingProject.cpp
+++ chattingProject.cpp
@@ -1,8 +1,134 @@
 ﻿#include <iostream>
-
+#include <string>
+#include <mysql/jdbc.h>
+#include <string>
 // 채팅 프로그램 - 메인.
 
+using namespace std;
 
-int main() {
+
+
+void join_membership(string id, string pw, string name, string phone)
+{
+    const string server = "tcp://127.0.0.1:3306"; // 데이터베이스 주소
+    const string username = "root"; // 데이터베이스 사용자
+    const string password = "0000"; // 데이터베이스 접속 비밀번호
+
+    // MySQL Connector/C++ 초기화
+    sql::mysql::MySQL_Driver* driver; // 추후 해제하지 않아도 Connector/C++가 자동으로 해제해 줌
+    sql::Connection* con;
+    sql::Statement* stmt;
+    sql::PreparedStatement* pstmt;
+
+    try {
+        driver = sql::mysql::get_mysql_driver_instance();
+        con = driver->connect(server, username, password);
+    }
+    catch (sql::SQLException& e) {
+        cout << "Could not connect to server. Error message: " << e.what() << endl;
+        exit(1);
+    }
+
+    // 데이터베이스 선택
+    con->setSchema("chattingproject");
+
+    // db 한글 저장을 위한 셋팅 
+    stmt = con->createStatement();
+    stmt->execute("set names euckr");
+    if (stmt) { delete stmt; stmt = nullptr; }
+
+    // 데이터베이스 쿼리 실행
+
+    // DROP table
+    /*stmt = con->createStatement();
+    stmt->execute("DROP TABLE IF EXISTS 테이블명");
+    cout << "Finished dropping table (if existed)" << endl;
+    */
+
+    // CREATE table
+    /*stmt->execute("CREATE TABLE 테이블명 (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);");
+    cout << "Finished creating table" << endl;
+    delete stmt;
+    */
+
+    // INSERT
+    pstmt = con->prepareStatement("INSERT INTO member(memberID, passWord, name, phoneNumber) VALUES(?,?,?,?)");
+
+    pstmt->setString(1, id);
+    pstmt->setString(2, pw);
+    pstmt->setString(3, name);
+    pstmt->setString(4, pw);
+    pstmt->execute();
+    cout << "One row inserted." << endl;
+
+    // MySQL Connector/C++ 정리
+    delete pstmt;
+    delete con;
+}
+
+
+int main()
+{  
+    int select, idCheck = 0, repeatedCheck = 0;
+    string id, pw, name, phone, allID;
+
+    while(true)
+    {
+        cout << "1. 로그인 2. 회원가입 : ";
+        cin >> select;
+
+        if (select != 1 && select != 2)
+        {
+            cout << "\n잘못 입력하셨습니다." << endl;
+        }
+
+        else if (select == 1) // 로그인 해서 채팅 입장
+        {
+
+        }
+
+        else if (select == 2) // 회원가입
+        {
+            while (idCheck == 0 || repeatedCheck == 0) // 영어 만 있는지 체크,  id 중복 체크
+            {
+                cout << "사용할 ID를 입력해 주세요(영어, 20자 이내) : ";
+                cin >> id;
+                
+                if (allID.find(id) != string::npos) // id 중복 체크
+                {
+                    cout << "중복된 ID가 있습니다." << endl;
+                    break;
+                }
+                else
+                {
+                    repeatedCheck = 1;
+
+                    for (int i = 0; i < size(id); i++) // 영어 만 있는지 체크
+                    {
+                        if (isalpha(id[i]) == false)
+                        {
+                            cout << "영어가 아닌 문자가 포함됐습니다." << endl;
+                            break;
+                        }
+                        else
+                        {
+                            idCheck = 1;
+                        }
+                    }
+                }
+            }
+
+            cout << "사용할 PW를 입력해 주세요(영어, 20자 이내) : ";
+            cin >> pw;
+
+            cout << "이름을 입력해 주세요(10자 이내) : ";
+            cin >> name;
+
+            cout << "전화번호를 입력해 주세요(슬래쉬포함) : ";
+            cin >> phone;
+
+            join_membership(id, pw, name, phone);
+        }
+    }
 
 }

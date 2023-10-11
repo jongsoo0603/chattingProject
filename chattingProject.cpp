69cbba1596224b600ecb63bf2967a5cc796bcedc
seguir0114 <hbinch9598@gmail.com>
2023-10-11 18:09:51 +0900

    clear func

--- chattingProject.cpp
+++ chattingProject.cpp
@@ -5,14 +5,13 @@
 
 // 채팅 프로그램 - 메인.
 
-
 using namespace std;
 
 const string server = "tcp://127.0.0.1:3306"; // 데이터베이스 주소
 const string username = "root"; // 데이터베이스 사용자
 const string password = "1122"; // 데이터베이스 접속 비밀번호
 
-void join_membership(string id, string pw, string name, string phone)
+void insertDB(string id, string pw, string name, string phone)
 {
     // MySQL Connector/C++ 초기화
     sql::mysql::MySQL_Driver* driver; // 추후 해제하지 않아도 Connector/C++가 자동으로 해제해 줌
@@ -53,7 +52,6 @@ void join_membership(string id, string pw, string name, string phone)
     delete con;
 }
 
-
 string makeAllID()
 {
     string allID;
@@ -80,7 +78,7 @@ string makeAllID()
     res = stmt->executeQuery("SELECT * FROM member");
 
     // ID 다 가져다 붙이기
-    while (res->next()) 
+    while (res->next())
     {
         allID += res->getString("memberID") + " ";
     }
@@ -93,6 +91,124 @@ string makeAllID()
     return allID;
 }
 
+void inputMembership()
+{
+    string id, name, phone, pw, allID = makeAllID();
+    bool isalphaYN = false, pwCheck = false, phoneCheck = false;
+
+    while (true)
+    {
+        cout << "사용할 ID를 입력해 주세요(영어+숫자, 20자 이내) : ";
+        cin >> id;
+        // cout << id << endl;
+        for (int i = 0; i < size(id); i++) // 영어, 숫자 만 있는지 체크
+        {
+            if (isalnum(id[i]) == 0)
+            {
+                isalphaYN = true;
+            }
+        }
+
+        if (isalphaYN)
+        {
+            cout << "아이디는 영어와 숫자 만 입력이 가능합니다." << endl;
+            isalphaYN = false;
+        }
+        else
+        {
+            if (allID.find(id) != string::npos) // id 중복 체크
+            {
+                cout << "중복된 ID가 있습니다." << endl;
+            }
+            else
+            {
+                while (pwCheck == false)
+                {
+                    cout << "사용할 PW를 입력해 주세요(숫자 6자리 만) : ";
+                    cin >> pw;
+                    cout << "입력값 : " << pw << endl;
+                    if (size(pw) != 6)
+                    {
+                        cout << "사이즈오류" << endl;
+                        pwCheck = true;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 6; i++)
+                        {
+                            if (isdigit(pw[i]) == 0)
+                            {
+                                cout << "숫자아님오류" << endl;
+                                pwCheck = true;
+                            }
+                        }
+                    }
+                    if (pwCheck)
+                    {
+                        cout << "비밀번호는 숫자 6자리 만 입력이 가능합니다." << endl;
+                        pwCheck = false;
+                    }
+                    else
+                    {
+                        pwCheck = true;
+                    }
+                }
+
+                while (true)
+                {
+                    cout << "이름을 입력해 주세요(10자 이하) : ";
+                    cin >> name;
+                    if (size(name) < 11)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        cout << "이름은 10자 이하여야 합니다." << endl;
+                    }
+
+                }
+
+                while (phoneCheck == false)
+                {
+                    cout << "전화번호를 입력해 주세요(슬래쉬제외 11자리) : ";
+                    cin >> phone;
+
+                    if (size(phone) != 11) // 11자리 체크
+                    {
+                        cout << "11자리 오류" << endl;
+                        phoneCheck = true;
+                    }
+                    else // 11자리면
+                    {
+                        for (int i = 0; i < size(phone); i++)
+                        {
+                            if (isdigit(phone[i]) == 0) // 숫자 체크
+                            {
+                                cout << "숫자아님오류" << endl;
+                                phoneCheck = true;
+                            }
+                        }
+                    }
+
+                    if (phoneCheck == false)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        cout << "전화번호는 11개의 숫자이어야 합니다." << endl;
+                        phoneCheck = false;
+                    }
+                }
+
+                insertDB(id, pw, name, phone);
+                break;
+            }
+        }
+
+    }
+}
 
 void inputLogin() {
     string inputId, inputPw;
@@ -161,9 +277,7 @@ void inputLogin() {
 
 int main()
 {  
-    int select, alphaCheck = 0, repeatedCheck = 0;
-    string id, name, phone, pw, allID = makeAllID();
-    bool isalphaYN = false, pwCheck = false, phoneCheck = false;
+    int select;
 
     while(true)
     {
@@ -183,119 +297,8 @@ int main()
 
         else if (select == 2) // 회원가입
         {
-            while (true)
-            {
-                cout << "사용할 ID를 입력해 주세요(영어+숫자, 20자 이내) : ";
-                cin >> id;
-                // cout << id << endl;
-                for (int i = 0; i < size(id); i++) // 영어, 숫자 만 있는지 체크
-                {
-                    if (isalnum(id[i]) == 0)
-                    {
-                        isalphaYN = true;
-                    }
-                }
-
-                if (isalphaYN) 
-                {
-                    cout << "아이디는 영어와 숫자 만 입력이 가능합니다." << endl;
-                    isalphaYN = false;
-                }
-                else 
-                {
-                    if (allID.find(id) != string::npos) // id 중복 체크
-                    {
-                        cout << "중복된 ID가 있습니다." << endl;
-                    }
-                    else
-                    {
-                        while (pwCheck == false)
-                        {
-                            cout << "사용할 PW를 입력해 주세요(숫자 6자리 만) : ";
-                            cin >> pw;
-                            cout << "입력값 : " << pw << endl;
-                            if (size(pw) != 6)
-                            {
-                                cout << "사이즈오류" << endl;
-                                pwCheck = true;
-                            }
-                            else
-                            {
-                                for (int i = 0; i < 6; i++)
-                                {
-                                    if (isdigit(pw[i]) == 0)
-                                    {
-                                        cout << "숫자아님오류" << endl;
-                                        pwCheck = true;
-                                    }
-                                }
-                            }
-                            if (pwCheck)
-                            {
-                                cout << "비밀번호는 숫자 6자리 만 입력이 가능합니다." << endl;
-                                pwCheck = false;
-                            }
-                            else
-                            {
-                                pwCheck = true;
-                            }
-                        }
-                        
-                        while (true)
-                        {
-                            cout << "이름을 입력해 주세요(10자 이하) : ";
-                            cin >> name;
-                            if (size(name) < 11)
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                cout << "이름은 10자 이하여야 합니다." << endl;
-                            }
-
-                        }
-                        
-
-                        while (phoneCheck == false)
-                        {
-                            cout << "전화번호를 입력해 주세요(슬래쉬제외 11자리) : ";
-                            cin >> phone;
-
-                            if (size(phone) != 11) // 11자리 체크
-                            {
-                                cout << "11자리 오류" << endl;
-                                phoneCheck = true;
-                            }
-                            else // 11자리면
-                            {
-                                for (int i = 0; i < size(phone); i++)
-                                {
-                                    if (isdigit(phone[i]) == 0) // 숫자 체크
-                                    {
-                                        cout << "숫자아님오류" << endl;
-                                        phoneCheck = true;
-                                    }
-                                }
-                            }
-                            
-                            if (phoneCheck == false)
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                cout << "전화번호는 11개의 숫자이어야 합니다." << endl;
-                                phoneCheck = false;
-                            }
-                        }
-                        
-                        join_membership(id, pw, name, phone);
-                        break;   
-                    }
-                }
-               
-            }
+            inputMembership();
+            break;
         }
     }
 

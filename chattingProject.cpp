5a984d47ea46458bf578ce7c9b3e3008fccf95c4
seguir0114 <hbinch9598@gmail.com>
2023-10-12 09:38:55 +0900

    add chatting func

--- chattingProject.cpp
+++ chattingProject.cpp
@@ -1,9 +1,15 @@
-﻿#include <iostream>
+﻿#pragma comment(lib, "ws2_32.lib")
+
+#include <iostream>
 #include <string>
 #include <mysql/jdbc.h>
+#include <WinSock2.h> //Winsock 헤더파일 include. WSADATA 들어있음.
+#include <WS2tcpip.h>
+#include <sstream>
+#include <thread>
 
+#define MAX_SIZE 1024
 
-// 채팅 프로그램 - 메인.
 
 using namespace std;
 
@@ -11,6 +17,76 @@ const string server = "tcp://127.0.0.1:3306"; // 데이터베이스 주소
 const string username = "root"; // 데이터베이스 사용자
 const string password = "1122"; // 데이터베이스 접속 비밀번호
 
+SOCKET client_sock;
+string my_nick;
+
+
+// 채팅 받아옴
+int chat_recv() {
+    char buf[MAX_SIZE] = { };
+    string msg;
+
+    while (1) {
+        ZeroMemory(&buf, MAX_SIZE);
+        if (recv(client_sock, buf, MAX_SIZE, 0) > 0) {
+            msg = buf;
+            std::stringstream ss(msg);  // 문자열을 스트림화
+            string user;
+            ss >> user; // 스트림을 통해, 문자열을 공백 분리해 변수에 할당. 보낸 사람의 이름만 user에 저장됨.
+            if (user != my_nick) cout << buf << endl; // 내가 보낸 게 아닐 경우에만 출력하도록.
+        }
+        else {
+            cout << "Server Off" << endl;
+            return -1;
+        }
+    }
+}
+
+void client(string inputId)
+{
+    WSADATA wsa;
+
+    // Winsock를 초기화하는 함수. MAKEWORD(2, 2)는 Winsock의 2.2 버전을 사용하겠다는 의미.
+    // 실행에 성공하면 0을, 실패하면 그 이외의 값을 반환.
+    // 0을 반환했다는 것은 Winsock을 사용할 준비가 되었다는 의미.
+    int code = WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    if (!code) {
+        my_nick = inputId;
+
+        client_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP); // 
+
+        // 연결할 서버 정보 설정 부분
+        SOCKADDR_IN client_addr = {};
+        client_addr.sin_family = AF_INET;
+        client_addr.sin_port = htons(7777);
+        InetPton(AF_INET, TEXT("127.0.0.1"), &client_addr.sin_addr); // 연결할 서버의 ip주소
+
+        while (1) {
+            if (!connect(client_sock, (SOCKADDR*)&client_addr, sizeof(client_addr))) { // 위에 설정한 정보에 해당하는 server로 연결!
+                cout << "Server Connect" << endl;
+                send(client_sock, my_nick.c_str(), my_nick.length(), 0); // 연결에 성공하면 client 가 입력한 닉네임을 서버로 전송
+                break;
+            }
+            cout << "Connecting..." << endl;
+        }
+
+        std::thread th2(chat_recv);
+
+        while (1) {
+            string text;
+            std::getline(cin, text);
+            const char* buffer = text.c_str(); // string형을 char* 타입으로 변환
+            send(client_sock, buffer, strlen(buffer), 0);
+        }
+        th2.join();
+        closesocket(client_sock);
+    }
+
+    WSACleanup();
+}
+
+// db에 회원가입 정보 삽입
 void insertDB(string id, string pw, string name, string phone)
 {
     // MySQL Connector/C++ 초기화
@@ -50,8 +126,9 @@ void insertDB(string id, string pw, string name, string phone)
     // MySQL Connector/C++ 정리
     delete pstmt;
     delete con;
-}
+} 
 
+// db에서 모든 id 받아와서 string으로 붙이기
 string makeAllID()
 {
     string allID;
@@ -89,8 +166,9 @@ string makeAllID()
     delete con;
 
     return allID;
-}
+} 
 
+// 회원가입 조건 확인
 void inputMembership()
 {
     string id, name, phone, pw, allID = makeAllID();
@@ -126,10 +204,8 @@ void inputMembership()
                 {
                     cout << "사용할 PW를 입력해 주세요(숫자 6자리 만) : ";
                     cin >> pw;
-                    cout << "입력값 : " << pw << endl;
                     if (size(pw) != 6)
                     {
-                        cout << "사이즈오류" << endl;
                         pwCheck = true;
                     }
                     else
@@ -138,7 +214,6 @@ void inputMembership()
                         {
                             if (isdigit(pw[i]) == 0)
                             {
-                                cout << "숫자아님오류" << endl;
                                 pwCheck = true;
                             }
                         }
@@ -210,6 +285,7 @@ void inputMembership()
     }
 }
 
+// 로그인 조건 확인
 void inputLogin() {
     string inputId, inputPw;
     bool login = false, idYN = false, pwYN = false;;
@@ -260,6 +336,7 @@ void inputLogin() {
     if (login) {
         //로그인 성공. - 채팅방 입장.
         cout << "로그인 성공 :: 채팅방 입장" << endl;
+        client(inputId);
     }
     else {
         //로그인 실패. - 다시 로그인 받기.
@@ -274,7 +351,7 @@ void inputLogin() {
 
 }
 
-
+// 채팅 프로그램 - 메인.
 int main()
 {  
     int select;
@@ -298,7 +375,6 @@ int main()
         else if (select == 2) // 회원가입
         {
             inputMembership();
-            break;
         }
     }
 
